23214d876ee84ab9ada0ecddf91559d7e9abbd61
Burton <rheutan7@gmail.com>
2016-08-13 20:02:39 -0500

    Initial commit of files

---
+++ src/dotnet-Core-Web-base/Models/ManageViewModels/FactorViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dotnet_Core_Web_base.Models.ManageViewModels
+{
+    public class FactorViewModel
+    {
+        public string Purpose { get; set; }
+    }
+}
---
+++ src/dotnet-Core-Web-base/Services/ISmsSender.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dotnet_Core_Web_base.Services
+{
+    public interface ISmsSender
+    {
+        Task SendSmsAsync(string number, string message);
+    }
+}
